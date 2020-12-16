4c0d8142148c9d154d6f3c332119f1adc06a4574
maisashour96 <45119395+maisashour96@users.noreply.github.com>
2020-12-17 01:07:19 +0200

    Create ubuntu.bash

---
+++ ubuntu.bash
@@ -0,0 +1,24 @@
+
+لبدر@DESKTOP-A11DEHJ MINGW64 ~/Desktop/code.py
+$ ls -l
+total 1
+-rw-r--r-- 1 لبدر 197121 62 Dec 17 00:46 code.py
+
+لبدر@DESKTOP-A11DEHJ MINGW64 ~/Desktop/code.py
+$ chmod o+w code.py
+
+لبدر@DESKTOP-A11DEHJ MINGW64 ~/Desktop/code.py
+$ ls -l
+total 1
+-rw-r--r-- 1 لبدر 197121 62 Dec 17 00:46 code.py
+
+لبدر@DESKTOP-A11DEHJ MINGW64 ~/Desktop/code.py
+$ chmod o-w code.py
+
+لبدر@DESKTOP-A11DEHJ MINGW64 ~/Desktop/code.py
+$ ls -l
+total 1
+-rw-r--r-- 1 لبدر 197121 62 Dec 17 00:46 code.py
+
+لبدر@DESKTOP-A11DEHJ MINGW64 ~/Desktop/code.py
+$
