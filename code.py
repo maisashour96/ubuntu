92770e15fa88a145959ee71f08e33f41eef918e2
maisashour96 <45119395+maisashour96@users.noreply.github.com>
2020-12-17 01:09:08 +0200

    Add files via upload

---
+++ code.py
@@ -0,0 +1,12 @@
+U -user
+g - group
+o - other people (from the outside world)
+
+chmod 754 code.py
+7, 5, 4 represents the individual for (in this order) user, group, other
+4 - stands for "read"
+2 - stands for "write"
+1 - stands for "execute"
+0 - stands for "no permissions"
+
+chmod 777 code.py
